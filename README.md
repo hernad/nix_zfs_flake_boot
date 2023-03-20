6a2a7ef2e1c6f89e16857338f7b7900da7cdba12
Ernad Husremovic <hernad@bring.out.ba>
2023-03-20 14:54:00 +0100

    unstable

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
     nix flake clone github:hernad/nix_zfs_flake_boot --dest hernad
     cd hernad
-    sudo nixos install --flake .#lenovo16
+
+    # sudo zpool import -f rpool bpool
+    # sudo bash scripts/mount_zfs.sh
+
+    sudo nixos-install --flake .#lenovo16
 
     sudo umount -Rl /mnt
     sudo zpool export -a
--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     # stable
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
-    home-manager.url = "github:nix-community/home-manager/release-22.11";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    # home-manager.url = "github:nix-community/home-manager/release-22.11";
 
     # unstable
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    # home-manager.url = "github:nix-community/home-manager";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
 
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
