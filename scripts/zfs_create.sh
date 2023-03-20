8fdd03d7529637b5025edf0e887c634496298804
Ernad Husremovic <hernad@bring.out.ba>
2023-03-20 15:12:19 +0100

    zpool zfs create

---
+++ scripts/zfs_create.sh
@@ -0,0 +1,66 @@
+zpool create -f \
+    -o compatibility=grub2 \
+    -o ashift=12 \
+    -o autotrim=on \
+    -O acltype=posixacl \
+    -O canmount=off \
+    -O compression=lz4 \
+    -O devices=off \
+    -O normalization=formD \
+    -O relatime=on \
+    -O xattr=sa \
+    -O mountpoint=/boot \
+    -R /mnt \
+    bpool \
+    /dev/nvme0n1p6
+
+
+zpool create -f \
+    -o ashift=12 \
+    -o autotrim=on \
+    -R /mnt \
+    -O acltype=posixacl \
+    -O canmount=off \
+    -O compression=zstd \
+    -O dnodesize=auto \
+    -O normalization=formD \
+    -O relatime=on \
+    -O xattr=sa \
+    -O mountpoint=/ \
+    rpool \
+    /dev/nvme0n1p7
+
+zfs create \
+  -o canmount=off \
+  -o mountpoint=none \
+  rpool/nixos
+
+
+zfs create -o mountpoint=legacy     rpool/nixos/root
+mount -t zfs rpool/nixos/root /mnt/
+zfs create -o mountpoint=legacy rpool/nixos/home
+mkdir /mnt/home
+mount -t zfs  rpool/nixos/home /mnt/home
+zfs create -o mountpoint=legacy  rpool/nixos/var
+
+mkdir -pv /mnt/nixos
+zfs create -o mountpoint=legacy rpool/nixos/nix
+
+zfs create -o mountpoint=legacy rpool/nixos/var/log
+zfs create -o mountpoint=none bpool/nixos
+zfs create -o mountpoint=legacy bpool/nixos/root
+mkdir /mnt/boot
+mount -t zfs bpool/nixos/root /mnt/boot
+zfs create -o mountpoint=legacy rpool/nixos/empty
+zfs snapshot rpool/nixos/empty@start
+
+
+mkfs.vfat -n EFI /dev/nvme0n1p5
+mkdir -p /mnt/boot/efis/nvme0n1p5
+mount -t vfat nvme0n1p5 /mnt/boot/efis/nvme0n1p5
+
+
+
+
+
+    
--- scripts/zpool_create.sh
+++ scripts/zpool_create.sh
@@ -30,37 +30,3 @@ zpool create -f \
     rpool \
     /dev/nvme0n1p7
 
-zfs create \
-  -o canmount=off \
-  -o mountpoint=none \
-  rpool/nixos
-
-
-zfs create -o mountpoint=legacy     rpool/nixos/root
-mount -t zfs rpool/nixos/root /mnt/
-zfs create -o mountpoint=legacy rpool/nixos/home
-mkdir /mnt/home
-mount -t zfs  rpool/nixos/home /mnt/home
-zfs create -o mountpoint=legacy  rpool/nixos/var
-
-mkdir -pv /mnt/nixos
-zfs create -o mountpoint=legacy rpool/nixos/nix
-
-zfs create -o mountpoint=legacy rpool/nixos/var/log
-zfs create -o mountpoint=none bpool/nixos
-zfs create -o mountpoint=legacy bpool/nixos/root
-mkdir /mnt/boot
-mount -t zfs bpool/nixos/root /mnt/boot
-zfs create -o mountpoint=legacy rpool/nixos/empty
-zfs snapshot rpool/nixos/empty@start
-
-
-mkfs.vfat -n EFI /dev/nvme0n1p5
-mkdir -p /mnt/boot/efis/nvme0n1p5
-mount -t vfat nvme0n1p5 /mnt/boot/efis/nvme0n1p5
-
-
-
-
-
-    
