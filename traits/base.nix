c4d497ae18563e36d90fa1b5176693d7f3505dcf
Ernad Husremovic <hernad@bring.out.ba>
2023-03-20 20:05:01 +0100

    libraries

--- traits/base.nix
+++ traits/base.nix
@@ -17,6 +17,7 @@
   environment.systemPackages = with pkgs;
     let
       inherit (pkgs) fetchurl;
+      inherit (lib) makeLibraryPath;
 
       vcsodeWithExtension = vscode-with-extensions.override {
         # When the extension is already available in the default extensions set.
@@ -49,6 +50,52 @@
             url = "https://download.cdn.viber.com/cdn/desktop/Linux/viber.deb";
             sha256 = "sha256-tjyvf1qjzznPO7YPreebo/CoqAn3fR/dfKuwT/Bm7/c=";
           };
+
+        libPath = lib.makeLibraryPath [
+              alsa-lib
+              cups
+              curl
+              dbus
+              expat
+              fontconfig
+              freetype
+              glib
+              gst_all_1.gst-plugins-base
+              gst_all_1.gst-plugins-bad
+              gst_all_1.gstreamer
+              harfbuzz
+              libcap
+              libGLU libGL
+              libpulseaudio
+              libxkbcommon
+              libxml2
+              libxslt
+              nspr
+              nss
+              openssl_1_1
+              stdenv.cc.cc
+              systemd
+              wayland
+              zlib
+              xorg.libICE
+              xorg.libSM
+              xorg.libX11
+              xorg.libxcb
+              xorg.libXcomposite
+              xorg.libXcursor
+              xorg.libXdamage
+              xorg.libXext
+              xorg.libXfixes
+              xorg.libXi
+              xorg.libXrandr
+              xorg.libXrender
+              xorg.libXScrnSaver
+              xorg.libXtst
+              xorg.xcbutilimage
+              xorg.xcbutilkeysyms
+              xorg.xcbutilrenderutil
+              xorg.xcbutilwm
+          ];  
       });
 
     in [
