38865f08b28768cf3d30de2a6a9876bafa7a56b9
Ernad Husremovic <hernad@bring.out.ba>
2023-04-10 15:01:40 +0200

    cleanup

--- scripts/part.sh
+++ scripts/part.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+installDisk=/dev/nvme0n1
+
 # first disk partition NixOS
 diskPart=4
-installDisk=/dev/nvme0n1
+
 # SWAP = 8GB
 INST_PARTSIZE_SWAP=8
 
@@ -21,6 +23,8 @@ P_SWAP=$diskPart
 #diskPart=$((diskPart+1))
 #P_BIOS=$diskPart
 
+echo installDisk=$installDisk
+echo "SWAP SIZE (GB)=$INST_PARTSIZE_SWAP"
 echo P_EFI=$P_EFI
 echo P_BOOT=$P_BOOT
 echo P_ROOT=$P_ROOT
