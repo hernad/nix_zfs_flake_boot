26b4bac8e86d4d70d73d5206ede115aac21b39e7
Ernad Husremovic <hernad@bring.out.ba>
2023-04-10 14:57:44 +0200

    part.sh, part_create.sh

--- scripts/zpool_create.sh
+++ scripts/zpool_create.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+if [ ! -f ./scripts/part.sh ]; then
+   echo ./scripts/part.sh not exists
+   exit 1
+fi
+
+. /scripts/part.sh
+
 zpool create -f \
     -o compatibility=grub2 \
     -o ashift=12 \
@@ -12,8 +21,7 @@ zpool create -f \
     -O mountpoint=/boot \
     -R /mnt \
     bpool \
-    /dev/nvme0n1p6
-
+    ${installDisk}p${P_BOOT}
 
 zpool create -f \
     -o ashift=12 \
@@ -28,5 +36,32 @@ zpool create -f \
     -O xattr=sa \
     -O mountpoint=/ \
     rpool \
-    /dev/nvme0n1p7
+    ${installDisk}p${P_ROOT}
 
+zfs create \
+ -o canmount=off \
+ -o mountpoint=none \
+ rpool/nixos
+
+zfs create -o mountpoint=legacy     rpool/nixos/root
+mount -t zfs rpool/nixos/root /mnt/
+zfs create -o mountpoint=legacy rpool/nixos/home
+mkdir /mnt/home
+mount -t zfs  rpool/nixos/home /mnt/home
+zfs create -o mountpoint=legacy  rpool/nixos/var
+zfs create -o mountpoint=legacy rpool/nixos/var/lib
+zfs create -o mountpoint=legacy rpool/nixos/var/log
+zfs create -o mountpoint=none bpool/nixos
+zfs create -o mountpoint=legacy bpool/nixos/root
+mkdir /mnt/boot
+mount -t zfs bpool/nixos/root /mnt/boot
+zfs create -o mountpoint=legacy rpool/nixos/empty
+zfs snapshot rpool/nixos/empty@start
+
+mkfs.vfat -n EFI ${installDisk}p${P_EFI}
+mkdir -p /mnt/boot/efis/${installDisk##*/}p${P_EFI}
+mount -t vfat ${installDisk}p${P_EFI} /mnt/boot/efis/${installDisk##*/}p${P_EFI}
+
+zpool list -v
+
+mount | grep vfat
